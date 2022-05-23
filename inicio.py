3893ef6eac88505fe8a285d2a4dda38a300a916e
Crhis Ferreira <cferreira18@unifesp.br>
2022-05-23 14:22:00 -0300

    aula 01 finalizada

--- inicio.py
+++ inicio.py
@@ -4,22 +4,21 @@ url = 'https://gist.githubusercontent.com/tgcsantos/3bdb29eba6ce391e90df2b72205b
 
 dados = pd.read_csv(url)
 
+dados    # mostra o número de linhas e colunas (resumo)
 
-#dados    # mostra o número de linhas e colunas (resumo)
+dados.head()    #acessa as primeiras n linhas
 
-#dados.head()    #acessa as primeiras n linhas
+dados.sample(10)   # retorna uma selecao aleatoria de linhas na quantidade escolhida
 
-#dados.sample(10)   # retorna uma selecao aleatoria de linhas na quantidade escolhida
+dados.info()     # informação dos dados (str, int, etc)
 
-#dados.info()     # informação dos dados (str, int, etc)
+dados["Bairro"][6522]   # buscando X coluna na linha X
 
-#dados["Bairro"][6522]   # buscando X coluna na linha X
+dados.Bairro   # outra maneira de puxar só X coluna
 
-#dados.Bairro   # outra maneira de puxar só X coluna
+dados.Metragem.mean()   #média da coluna X que é int
 
-#dados.Metragem.mean()   #média da coluna X que é int
-
-#dados["Metragem"].mean()  #achando média de colunas com nomes ruins
+dados["Metragem"].mean()  #achando média de colunas com nomes ruins
 
 #-------------------------
 #QUANTOS IMOVEIS EU TENHO NA VILA MARIANA??
@@ -30,3 +29,21 @@ qtde_imoveis_vila_mariana = sum(dados["Bairro"] == "Vila Mariana")  #quantos imo
 imoveis_vila_mariana = dados[tem_imoveis_vila_mariana]   
  
 print(imoveis_vila_mariana["Metragem"].mean())
+
+
+
+#quantidade de imoveis por bairro
+numero_imoveis_bairro = dados["Bairro"].value_counts()   
+numero_imoveis_bairro.head(10).plot.bar()
+
+
+
+# 1 - Realizar a média da metragem para cara um dos bairros (Paulo)
+
+# 2 formas de selecionar os dados por bairro (consultar os métodos na documentação do pandas) (Thiago)
+
+# 3 - Explorar alguns gráficos na documentação e aplicar nas demais colunas do DF, tentar colocar alguma conclusão (Thiago)
+
+# 4 - Pegar outros estatísticas dos dados, média, mediana, mim, max (Vivi)
+
+# 5 - Descobrir quais são os bairros que não tem nome de Rua. (Vivi)
